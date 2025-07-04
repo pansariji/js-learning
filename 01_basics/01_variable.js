04ea424d4933263803fd79020c26b1eba7b675ab
Madhav Aggarwal <madhavaggarwal4016@gmail.com>
2025-07-04 08:49:11 +0000

    datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "madhavaggarwal0001@gmail.com"
+var accountPassword = "12345"    
+/* 
+var is not used anymore due to scope issue
+*/
+accountCity = "haridwar"
+
+// accountId = 2  #not allowed
+accountEmail = "hc@hc.com"
+accountPassword = "212121"
+accountCity = "roorkee"
+console.log(accountId);
+console.table([accountEmail, accountPassword, accountCity, accountId])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,11 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3)  // WE ARE USING NODEJS NOT BROWSER
+
+/*
+most of the data types are same as python
+null=> standalone value
+undefined 
+symbol => unique
+object
+*/
