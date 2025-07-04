0d5ea0dc371024d45e7e468a9a634cf041ed7963
Madhav Aggarwal <madhavaggarwal4016@gmail.com>
2025-07-04 08:58:06 +0000

    hh

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,6 +1,6 @@
 "use strict"; //treat all js code as newer version
 
-//alert(3+3)  // WE ARE USING NODEJS NOT BROWSER.
+//alert(3+3) // WE ARE USING NODEJS NOT BROWSER.
 
 /*
 most of the data types are same as python
