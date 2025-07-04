edb2bcf80fb1c2dbc5dc95d60eaf9e1f35afc0ca
Madhav Aggarwal <madhavaggarwal4016@gmail.com>
2025-07-04 09:04:54 +0000

    bhbhbhbhb

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,9 +1,9 @@
 "use strict"; //treat all js code as newer version 
 
-//alert(3+3) // WE ARE USING NODEJS NOT BROWSER.
+//alert(3+3) // WE ARE USING NODEJS NOT BROWSER.    
 
 /*
-most of the data types are same as python
+most of the data types are same as python 
 null=> standalone value
 undefined 
 symbol => unique
