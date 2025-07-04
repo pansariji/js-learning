31337224d986836c1bef42ea567360ca88ac3a90
Madhav Aggarwal <madhavaggarwal4016@gmail.com>
2025-07-04 12:07:12 +0000

    conversions

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = "33abc"
+console.log(typeof score);
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber); 
+console.log(valueInNumber); 
+//33 => 33
+// "33abc" => NaN (Not a Number)
+
+
+let marks = null
+console.log(typeof marks);
+let valueInNumbers = Number(marks)
+console.log(typeof valueInNumbers);
+console.log(valueInNumbers)
+// null => 0
+
+
+let isLoggedIn = true
+console.log(typeof isLoggedIn);
+let valueInNumberss = Number(isLoggedIn)
+console.log(typeof valueInNumberss);
+console.log(valueInNumberss)
+// true => 1
+// false => 0
+
+let bolleanIsLoggedIn = 0 
+let valueInBoolean = Boolean(bolleanIsLoggedIn)
+console.log(typeof valueInBoolean);
+console.log(valueInBoolean);
+// 0=> false , 1=>true
+// "" => false, "abc" => true
