4f4335267b45e13eef0ec4a73e36972872fca7a5
pansariji <madhavaggarwal4016@gmail.com>
2025-09-01 22:20:07 +0530

    memory

--- 01_basics/06_memory.js
+++ 01_basics/06_memory.js
@@ -1,10 +1,21 @@
 // Stack - use in primitive datatypes
 // Heap - use in Refrence datatypes
 
-
+// --------stack--------
 let myName = "Madhav Aggarwal"
+let friendName = "amrit"
+friendName = "Deepak"
+console.log(myName)
+console.log(friendName)
+// in stack u get copy of values so on changing second variable first variable doesn't change.
 
-
-
-
-// hi my name is madhav
+// --------heap--------
+let userOne = {
+    name: "Madhav Aggarwal",
+    age: 25
+}
+let userTwo = userOne
+userTwo.name = "Deepak"
+console.log(userOne.name)
+console.log(userTwo.name)
+// in heap u get reference of object so on changing second variable first variable also changes.
